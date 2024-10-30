4d433aaad5949d1152cabde54747f8ba54013db8
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-10-30 00:34:50 -0500

    4. Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,52 @@
+import Currency from "./3-currency";
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  /** Amount */
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  /** Currency */
+  set currency(currency) {
+    if (currency instanceof Currency === false) {
+      throw new TypeError ('currency must be an instance of "Currency"');
+    }
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  /** Display Full Price */
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  /** Convert Price */
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
