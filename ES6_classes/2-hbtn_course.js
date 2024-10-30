029447fcbe549452810c75928cadeaa68e0d4229
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-10-29 21:04:37 -0500

    2. A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+class HolbertonCourse {
+  constructor (name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+   * Name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * Length
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * Students
+   */
+  set students(students) {
+    if (students instanceof Array == false) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
+
+export default HolbertonCourse;
