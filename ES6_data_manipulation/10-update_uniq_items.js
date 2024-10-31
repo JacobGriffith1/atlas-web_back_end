52afef33fb4ef54ea0bae34af22a3bfb21d80625
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-10-31 17:01:56 -0500

    changed forEach

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems() {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  map.array.forEach((value, key) => {
+  map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
