a272ffe0b4a5e384fa3ccc13c8eb5609feb3d262
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-10-31 16:27:05 -0500

    linter

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,10 +3,9 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-  const buffer = new DataView(
-    new ArrayBuffer(length), 0, length,
-  );
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
 
   buffer.setUint8(position, value);
+
   return buffer;
 }
