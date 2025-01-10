c619c41534e82ae16e0ed9e699696a5b8edabd7b
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-10 00:14:52 -0600

    1. Concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+'''
+## 1. Basic Annotations - Concat
+Write a type-annotated function ```concat```
+that takes a string ```str1``` and a string ```str2```
+as arguments and returns a concatenated string.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Type-annotated function that takes two strings
+    and returns them as a concatenated string
+
+    @str1: First string
+    @str2: Second string
+    Return: Concatenated string composed of variable strings
+    '''
+    return str1 + str2
