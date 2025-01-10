e89dceb318984d803f28409ff230637949c8fa6d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-10 00:24:47 -0600

    3. String

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+'''
+## 3. Basic Annotations - To String
+Write a type-annotated function ```to_str```
+that takes a float ```n``` as an argument
+and returns the string representation of the float.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Type-annotated function that takes a float
+    and returns it as a str
+
+    @n: Float
+    Return: String representation of n
+    '''
+    return str(n)
