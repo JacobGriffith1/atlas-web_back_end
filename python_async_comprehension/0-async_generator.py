aa4d9a4177e28e7a91d45650dcdea17afcf79ec7
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-12 23:39:45 -0600

    psv

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,9 +11,9 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     '''
-    Function creates a generator that yields a 
+    Function creates a generator that yields a
     random number between 0 and 10
     '''
     for _ in range(10):
