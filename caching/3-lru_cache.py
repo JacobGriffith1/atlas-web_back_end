85aa821616b11e57567e9b8750049474276c8dd0
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-25 18:15:50 -0600

    back to brackets

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -39,5 +39,5 @@ class LRUCache(BaseCaching):
         if key and key in self.keys:
             self.keys.remove(key)
             self.keys.append(key)
-            return self.cache_data(key)
+            return self.cache_data[key]
         return None
