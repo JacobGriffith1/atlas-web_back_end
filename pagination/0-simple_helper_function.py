53d35e6078b1e72671ef4357b39c0ed3c1b05c14
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-25 20:09:25 -0600

    0. Helper

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+'''
+Module for a pagination helper function
+'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Helper function for pagination
+
+    @page: The page number
+    @page_size: The amount of results per page
+    Return: A tuple of the starting and end indexes for that page
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
