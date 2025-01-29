ae20cd45e2b94742a9daedc63805eab5d445c851
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-29 00:54:42 -0600

    0. regexing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+'''
+MODULE FOR FILTERING AND LOGGING INFORMATION
+'''
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    '''
+    Function returns a log message obfuscated
+    '''
+
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator,
+                         message)
+        return message
