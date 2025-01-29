87beba80b90e46082c35a1a53155c33cca748a62
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-29 02:10:08 -0600

    passhash

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+'''
+MODULE FOR PASSWORD ENCRYPTION
+'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''
+    Function hashes password
+    '''
+    encoded_pw = password.encode()
+    hashed_pw = bcrypt.hashpw(encoded_pw, bcrypt.gensalt())
+
+    return hashed_pw
