84510dae825c9715f9c688177ed3436841994d98
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-30 12:41:55 -0600

    pcs

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -49,13 +49,13 @@ class SessionAuth(Auth):
         '''
         if request is None:
             return False
-        
+
         session_id = self.session_cookie(request)
         if not session_id:
             return False
-        
+
         if not self.user_id_for_session_id(session_id):
             return False
-        
+
         del self.user_id_by_session_id[session_id]
         return True
