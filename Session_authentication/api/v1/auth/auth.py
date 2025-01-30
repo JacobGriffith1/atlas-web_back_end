38145d40c14687edc7b1c4b0ad0d9215c294ab2c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-01-29 22:09:13 -0600

    copied basic auth into session auth

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''
+MODULE FOR API AUTHENTICATION
+'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    '''
+    Authentication class
+    '''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        Checks if authentication is required
+        '''
+        if path is None or excluded_paths is None or excluded_paths == '':
+            return True
+        elif path[len(path) - 1] is not '/':
+            path += '/'
+        for item in excluded_paths:
+            astr = item.find("*")
+            if astr != -1 and len(path) >= len(item):
+                cpyPath = path[: astr]
+                if cpyPath == item[: astr]:
+                    return False
+            elif path == item:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''
+        Returns auth header
+        '''
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        Returns current user info
+        '''
+        return None
