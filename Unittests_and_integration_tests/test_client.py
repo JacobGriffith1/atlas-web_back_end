9f446eb7a890220800a5df8e14b1a863d6468c9e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-05 02:55:34 -0600

    5. Mock

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Test client.py
 '''
 from client import GithubOrgClient
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 
@@ -24,3 +24,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             GithubOrgClient.ORG_URL.format(org=org_url)
         )
+
+    def test_public_repos_url(self):
+        '''Test that _public_repos_url returns mocked payload
+        '''
+        expected = "https://test.com/test"
+        payload: dict = {"repos_url": expected}
+        mockme = 'client.GithubOrgClient.org'
+        with patch(mockme, PropertyMock(return_value=payload)):
+            qry = GithubOrgClient("test")
+            rsp = qry._public_repos_url
+
+            self.assertEqual(rsp, expected)
