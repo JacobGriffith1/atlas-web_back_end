b9d5f25cc747301435c4938b8dd23ce6a5dc98db
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-05 02:02:19 -0600

    rewrite attempt, checking if I can find what's wrong

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -46,6 +46,7 @@ class TestGetJson(unittest.TestCase):
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
+        result = get_json(test_url)
 
         mock_get.assert_called_once_with(test_url)
-        self.assertEqual(get_json(test_url), test_payload)
+        self.assertEqual(result, test_payload)
