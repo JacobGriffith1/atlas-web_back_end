c41b3c77b582ef5f32e241ead5c3ddf9a7c11e32
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-05 23:59:38 -0600

    6. more patch

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -36,3 +36,24 @@ class TestGithubOrgClient(unittest.TestCase):
             rsp = qry._public_repos_url
 
             self.assertEqual(rsp, expected)
+    
+    @patch("client.get_json")
+    def test_public_repos(self, get_json_mock):
+        '''Test public_repos for expected output
+        '''
+        expected: str = "http://test.com/test"
+        val1: str = "ASTR"
+        val2: str = "GEOS"
+        get_json_mock.return_value = ([
+            {"name": val1},
+            {"name": val2}
+        ])
+        with patch.object(
+            GithubOrgClient, "_public_repos_url", mockme=PropertyMock
+        ) as _public_repos_url_mock:
+            _public_repos_url_mock.return_value = expected
+            qry = GithubOrgClient("test")
+            rsp = qry.public_repos()
+
+            self.assertEqual(rsp, [val1, val2])
+            get_json_mock.assert_called_once()
