b08d534a2bde68d92ec22c4cfd6fabd241aa7a74
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-18 22:15:10 -0600

    3. @call_history

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,6 +17,19 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    '''Decorator stores the history of i/o for a particular function'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''Wrapper for call_history decorator'''
+        inputs = str(args)
+        self._redis.rpush(method.__qualname__ + ':inputs', inputs)
+
+        outputs = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ':outputs', outputs)
+        return outputs
+    return wrapper
+
 class Cache:
     '''Caching class'''
 
@@ -26,6 +39,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         Method generates a random key, stores the input data in Redis using
