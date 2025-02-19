f533935391a2930d37249a5e1663f02f428a7bf1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-18 21:11:23 -0600

    1. get, get_str, get_int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Redis Basic'''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -21,3 +21,29 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''
+        Method takes a 'key' string argument and an optional 'Callable'
+        argument named 'fn'. This callable will be used to convert the data
+        back to the desired format
+        '''
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        '''Parameterizes a value from redis to str'''
+        val = self._redis.get(key)
+        return val.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''Parameterizes a value from redis to int'''
+        val = self._redis.get(key)
+        try:
+            val = int(val.decode('utf-8'))
+        except Exception:
+            val = 0
+        return val
