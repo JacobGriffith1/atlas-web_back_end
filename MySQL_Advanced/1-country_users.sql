14c2b01b51d8bd271da13a59e61065d96b95f91a
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-02-19 21:02:03 -0600

    1. country

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- 1. In and Not Out
+-- Creates a table "users"
+-- Attributes: id, email, name, country
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL
+);
