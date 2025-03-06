3c882bfee8ceb43b64adfc2e8d4e9d6365c86f33
JacobGriffith1 <jacobgriffith6339@gmail.com>
2025-03-05 22:03:58 -0600

    0

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('Calculator', () => {
+    it('Numbers not rounded', () => {
+        const result = calculateNumber(1, 3);
+        assert.equal(result, 4);
+    });
+
+    it('Number rounded up', () => {
+        const result = calculateNumber(1, 3.7);
+        assert.equal(result, 4);
+    });
+
+    it('Numbers round down and up', () => {
+        const result = calculateNumber(1.2, 3.7);
+        assert.equal(result, 5);
+    });
+
+    it('Numbers both round up', () => {
+        const result = calculateNumber(1.5, 3.7);
+        assert.equal(result, 6);
+    });
+});
